72aae87a70b651069da9a4b0697ede079c570471
tg2021 <vldzlqn1230@naver.com>
2021-06-02 08:47:56 +0900

    githubtest

--- Spring/spring1/src/main/java/com/koreait/spring/user/UserController.java
+++ Spring/spring1/src/main/java/com/koreait/spring/user/UserController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
     @RequestMapping("/login")
     public String login() {
         return "user/login";
     }
-
 }
